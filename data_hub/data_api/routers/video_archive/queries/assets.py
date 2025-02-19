b118e5c656df7045c851fd79ba40adf50df3f51f
Tannous Geagea <tannous.geagea@wasteant.com>
2025-02-19 16:39:10 +0100

    added video archive to api

---
+++ data_hub/data_api/routers/video_archive/queries/assets.py
@@ -0,0 +1,257 @@
+
+import os
+import math
+import time
+import django
+django.setup()
+from django.db import connection
+from datetime import datetime, timedelta
+from datetime import date, timezone
+from typing import Callable
+from fastapi import Request
+from fastapi import Response
+from fastapi import APIRouter
+from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.routing import APIRoute
+from fastapi import status
+from pydantic import BaseModel, Field, create_model, ValidationError
+from typing import Dict, List, Optional
+from acceptance_control.models import (
+    VideoArchive,
+    VideoArchiveMedia,
+    )
+
+from metadata.models import (
+    TableType,
+    Language,
+    TenantTable,
+    TenantTableAsset,
+    TableAssetItem,
+    TableAssetLocalization,
+    TableAssetItemLocalization,
+    TenantTableFilter,
+    TenantTableAssetItem,
+)
+
+from tenants.models import (
+    TenantStorageSettings,
+)
+
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+AzAccoutKey = os.getenv('AzAccoutKey')
+
+class TimedRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+        async def custom_route_handler(request: Request) -> Response:
+            before = time.time()
+            response: Response = await original_route_handler(request)
+            duration = time.time() - before
+            response.headers["X-Response-Time"] = str(duration)
+            print(f"route duration: {duration}")
+            print(f"route response: {response}")
+            print(f"route response headers: {response.headers}")
+            return response
+
+        return custom_route_handler
+
+
+router = APIRouter(
+    route_class=TimedRoute,
+)
+
+def create_filter_model(fields: Dict[str, Optional[str]]):
+    """
+    This function creates a dynamic Pydantic model based on the fields provided,
+    without setting default values.
+    """
+    # Each field is optional but without a default value
+    return create_model("DynamicFilterModel", **{k: (Optional[str], ...) for k in fields})
+
+description = """
+
+This API endpoint is designed to retrieve detailed information related to a delivery identified by delivery_id. Here's a breakdown of its functionality:
+Endpoint Definition
+
+python
+
+@router.api_route(
+    "/video_archive/assets/{event_uid}", methods=["GET"], tags=["Delivery"]
+)
+
+    Route: /delivery/assets/{delivery_id}
+    Methods: GET
+    Tags: ["Delivery"]
+    Parameters:
+        delivery_id: Path parameter representing the unique identifier of the delivery.
+
+Functionality
+
+    Input Validation:
+        Checks if delivery_id is 'null'. If true, returns a 400 Bad Request response indicating that delivery_id cannot be null.
+        Checks if delivery_id is not a digit. If true, returns a 400 Bad Request response indicating that delivery_id should be a number.
+
+    Existence Check:
+        Queries the database (DeliveryState model) to verify if a delivery with the specified delivery_id exists. If not found, returns a 404 Not Found response indicating that the delivery was not found.
+
+    Successful Response:
+        If the delivery_id is valid and exists, constructs a detailed response containing:
+            Information about the delivery ('delivery' section).
+            Analytics related to the delivery ('analytics' section), including querying external APIs (query_impurity_by_delivery, query_staub_by_delivery, query_hotspot_by_delivery).
+            Additional information ('information' section) related to the delivery, such as comments and additional data.
+
+    Error Handling:
+        Catches exceptions:
+            HTTPException: Specifically catches and handles HTTP exceptions, setting a 404 Not Found status code.
+            General Exception: Catches any other unexpected errors, setting a 500 Internal Server Error status code and providing details of the error in the response.
+
+Response Structure
+
+    Success Response (200 OK):
+        Detailed JSON structure containing sections for delivery details, analytics, and additional information.
+
+    Error Responses:
+        400 Bad Request: When delivery_id is 'null' or not a digit.
+        404 Not Found: When the specified delivery_id does not exist in the database or when caught HTTPException.
+        500 Internal Server Error: For any other unexpected errors, providing details of the error in the response.
+
+Usage
+
+This API is used to fetch comprehensive information and analytics related to a specific delivery identified by delivery_id, integrating with external analytics APIs to provide enriched data about the delivery process.
+
+"""
+
+
+@router.api_route(
+    "/video_archive/assets/{event_uid}", methods=["GET"], tags=["Video Archive"], description=description,
+)
+def get_video_archive_assets(response: Response, event_uid:str, language:str=None):
+    results = {}
+    try:
+        if event_uid == 'null':
+            results['error'] = {
+                'status_code': "bad-request",
+                'status_description': "delivery_id is not supposed to be null",
+                'detail': 'delivery_id is null, please provide a valid delivery_id'
+            }
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return results
+    
+        if not VideoArchive.objects.filter(video_id=event_uid).exists():
+            results['error'] = {
+                'status_code': "Not-Found",
+                'status_description': f"event_uid {event_uid} is not found",
+                'detail': 'please provide a valid delivery_id'
+            }
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return results
+            
+        video_archive = VideoArchive.objects.get(video_id=event_uid)
+        video_archive_media = VideoArchiveMedia.objects.filter(video_archive=video_archive)
+
+        tenant = video_archive.tenant
+
+        msg = f'using given language {language}'
+        if not language:
+            lang_code = tenant.default_language
+            if lang_code:
+                language = lang_code
+                msg = f'using default language: {language}'
+            else:
+                language = 'de'
+                msg = f"using german language"
+        
+        if not Language.objects.filter(code=language).exists():
+            results = {
+                "error": {
+                    "status_code": "not found",
+                    "status_description": f"Given Language {language} not supported",
+                    "detail": f"Given Language {language} not supported! Supported Language: {[lang.name for lang in Language.objects.all()]}",
+                }
+            }    
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return results
+        
+        language = Language.objects.get(code=language)
+        table_assets = TenantTableAsset.objects.filter(
+            tenant_table=TenantTable.objects.get(
+                tenant=tenant,
+                table_type=TableType.objects.get(name='video_archive'),
+            ),
+            is_active=True,
+        )
+        
+        AzAccoutKey = TenantStorageSettings.objects.get(tenant=tenant).account_key
+        placeholder = {
+            'url': f"https://wacoreblob.blob.core.windows.net/amk/placeholder.jpg?{AzAccoutKey}",
+            'name': "Bild in Vorbereitung",
+            'time': (datetime.now() + timedelta(hours=2)).strftime(DATETIME_FORMAT),
+            }
+        
+        
+        categories = []
+        data = []
+        
+        for table_asset in table_assets:
+            categories.append(
+                {
+                    'key': table_asset.table_asset.key,
+                    'name': TableAssetLocalization.objects.get(asset=table_asset.table_asset, language=language).title if TableAssetLocalization.objects.filter(asset=table_asset.table_asset, language=language) else table_asset.table_asset.key,
+                }
+            )
+            
+            items = []
+            table_asset_items = TenantTableAssetItem.objects.filter(tenant_table_asset=table_asset, is_active=True)
+            for item in table_asset_items:
+                item = item.asset_item
+                items.append(
+                    {
+                        'key': item.key,
+                        'title': TableAssetItemLocalization.objects.get(asset_item=item, language=language).title if TableAssetItemLocalization.objects.filter(asset_item=item, language=language) else item.name,
+                        'type': item.media_type,
+                        'data': [
+                            {
+                                'url': f"{media.media.media_url}?{AzAccoutKey}",
+                                'name': media.media.media_name,
+                                'time': media.media.created_at.strftime(DATETIME_FORMAT),
+                            } for media in video_archive_media if media.media.media_type == item.media_type
+                        ] if len(video_archive_media) else [placeholder]
+                    },
+                )
+            
+            data.append(
+                {
+                    "key": table_asset.table_asset.key,
+                    "title":  TableAssetLocalization.objects.get(asset=table_asset.table_asset, language=language).title if TableAssetLocalization.objects.filter(asset=table_asset.table_asset, language=language) else table_asset.table_asset.key,
+                    "items": items,
+                }
+            )
+        
+        results = {
+            "msg": msg,
+            "categories": categories,
+            "data": data,
+        }
+        
+        
+        connection.close()
+        return results    
+    
+    except HTTPException as e:
+        results['error'] = {
+            "status_code": 404,
+        }
+        
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return results
+    
+    except Exception as e:
+        results['error'] = {
+            'status_code': 500,
+            "status_description": "Internal Server Error",
+            "detail": str(e),
+        }
+        
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return results
---
+++ data_hub/data_api/routers/video_archive/queries/data.py
@@ -0,0 +1,312 @@
+import os
+import json
+import time
+import math
+import django
+from django.db import connection
+from django.db.models import Q
+from fastapi import status
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+from datetime import time as dtime
+from typing import Callable
+from fastapi import Request
+from fastapi import Response
+from fastapi import APIRouter
+from fastapi import HTTPException, Query, Body, Form
+from fastapi.routing import APIRoute
+from typing import Dict, List, Optional
+from pydantic import BaseModel, Field, create_model, ValidationError
+from common_utils.timezone_utils.timeloc import (
+    get_location_and_timezone,
+    convert_to_local_time,
+)
+
+timezone_str = get_location_and_timezone()
+
+django.setup()
+from django.core.exceptions import ObjectDoesNotExist
+from tenants.models import (
+    Tenant,
+    PlantEntity,
+    TenantStorageSettings,
+)
+
+from acceptance_control.models import (
+    VideoArchive
+    )
+
+from metadata.models import (
+    TableType,
+    TenantTable,
+    Language,
+    TenantTableFilter,
+    PlantEntityLocalization,
+)
+
+def filter_mapping(key, value, tenant):
+    try:
+        if value is None:
+            return None
+        
+        if not len(value):
+            return None
+        
+        if value == "all":
+            return None
+
+        if key == "location":
+            return ("entity", PlantEntity.objects.get(entity_uid=value, entity_type__tenant=tenant))
+
+    except Exception as err:
+        raise ValueError(f"Failed to map filter value {value} filter {key}: {err}")
+
+class TimedRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+        async def custom_route_handler(request: Request) -> Response:
+            before = time.time()
+            response: Response = await original_route_handler(request)
+            duration = time.time() - before
+            response.headers["X-Response-Time"] = str(duration)
+            print(f"route duration: {duration}")
+            print(f"route response: {response}")
+            print(f"route response headers: {response.headers}")
+            return response
+
+        return custom_route_handler
+    
+
+router = APIRouter(
+    route_class=TimedRoute,
+)
+
+def create_filter_model(fields: Dict[str, Optional[str]]):
+    """
+    This function creates a dynamic Pydantic model based on the fields provided,
+    without setting default values.
+    """
+    return create_model("DynamicFilterModel", **{k: (Optional[str], v) for k, v in fields.items()})
+
+description = """
+    URL Path: /alarm
+
+    TO DO
+
+"""
+
+@router.api_route(
+    "/video_archive", methods=["GET"], tags=["Video Archive"], description=description,
+)
+def get_video_archive_data(
+    response: Response, 
+    tenant_domain:str,
+    user_filters: Optional[str] = Query(None),
+    from_date:datetime=None,
+    to_date:datetime=None,
+    items_per_page:int=15,
+    page:int=1,
+    language:str='de',
+    ):
+    results = {}
+    try:
+        
+        if not Language.objects.filter(code=language).exists():
+            results = {
+                "error": {
+                    "status_code": "not found",
+                    "status_description": f"Given Language {language} not supported",
+                    "detail": f"Given Language {language} not supported! Supported Language: {[lang.name for lang in Language.objects.all()]}",
+                }
+            }
+            
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return results
+        
+        filters_dict = {}
+        if user_filters:
+            try:
+                filters_dict = json.loads(user_filters)
+            except json.JSONDecodeError:
+                raise HTTPException(status_code=400, detail="Invalid JSON format for user_filters")
+
+        if not Tenant.objects.filter(domain=tenant_domain).exists():
+            results = {
+                "error": {
+                    "status_code": "not found",
+                    "status_description": f"Tenant {tenant_domain} not found",
+                    "detail": f"Tenant {Tenant} not found ! Existing options: {[tenant.domain for tenant in Tenant.objects.all()]}",
+                }
+            }
+            
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return results
+        
+        if not TableType.objects.filter(name='video_archive').exists():
+            results = {
+                "error": {
+                    "status_code": "not found",
+                    "status_description": f"Table Type alarm not found",
+                    "detail": f"Table Type alarm not found ! Existing options: {[table_type.name for table_type in TableType.objects.all()]}",
+                }
+            }
+            
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return results
+        
+        language = Language.objects.get(code=language)
+        table_type = TableType.objects.get(name='video_archive')
+        tenant = Tenant.objects.get(domain=tenant_domain)
+        AzAccoutKey = TenantStorageSettings.objects.get(tenant=tenant).account_key
+        
+        if not TenantTable.objects.filter(tenant=tenant, table_type=table_type):
+            results = {
+                "error": {
+                    "status_code": "not found",
+                    "status_description": f"Table Type video_archive not defined for tenant {tenant_domain}",
+                    "detail": f"Table Type video_archive not found for {tenant_domain}!"
+                }
+            }
+            
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return results
+        
+        today = datetime.today()
+        if from_date is None:
+            from_date = datetime(today.year, today.month, today.day)
+        
+        if to_date is None:
+            to_date = from_date
+            
+        from_date = from_date.replace(tzinfo=timezone.utc)
+        to_date = datetime.combine(to_date, dtime.max).replace(tzinfo=timezone.utc)
+        
+        if page < 1:
+            page = 1
+        
+        if items_per_page<=0:
+            results['error'] = {
+                'status_code': 400,
+                'status_description': f'Bad Request, items_per_pages should not be 0',
+                'detail': "division by zero."
+            }
+
+            response.status_code = status.HTTP_400_BAD_REQUEST    
+            return results
+        
+        tenant_table = TenantTable.objects.get(
+            tenant=tenant,
+            table_type=table_type
+        )
+        
+        tenant_table_filter = TenantTableFilter.objects.filter(
+            tenant_table=tenant_table,
+            is_active=True
+        )
+        
+        filter_fields = {
+            fil.table_filter.filter_name: fil.default for fil in tenant_table_filter
+        }
+        
+        
+        DynamicFilterModel = create_filter_model(filter_fields)
+        try:
+            validated_filters = DynamicFilterModel(**filters_dict)
+        except ValidationError as e:
+            results['error'] = {
+                "status_code": 422,
+                "detail": f"{e.errors()}"
+            }
+            
+            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+            return results
+
+        lookup_filters = Q()
+        lookup_filters &= Q(tenant=tenant)
+        lookup_filters &= Q(created_at__range=(from_date, to_date ))
+        for key, value in validated_filters:
+            filter_map = filter_mapping(key, value, tenant)
+            if filter_map:
+                lookup_filters &= Q(filter_map) 
+        
+        rows = []
+        video_archives = VideoArchive.objects.filter(lookup_filters).order_by('-created_at')
+        for video_archive in video_archives[(page - 1) * items_per_page:page * items_per_page]:
+            plant_entity = PlantEntity.objects.get(entity_uid=video_archive.entity.entity_uid, entity_type__tenant=tenant)
+            
+            if not PlantEntityLocalization.objects.filter(
+                plant_entity=plant_entity,
+                language=language,
+            ).exists():
+                results['error'] = {
+                    'status_code': "non-matching-query",
+                    'status_description': f'localization {language.name} not found for {plant_entity.entity_uid}',
+                    'detail': f'localization {language.name} not found for {plant_entity.entity_uid}',
+                }
+
+                response.status_code = status.HTTP_404_NOT_FOUND
+                return results
+
+            plant_entity_localization = PlantEntityLocalization.objects.get(
+                plant_entity=PlantEntity.objects.get(entity_uid=video_archive.entity.entity_uid, entity_type__tenant=tenant),
+                language=language,
+            )
+            
+            row = {
+                "id": video_archive.id,
+                "event_uid": video_archive.video_id,
+                "event_date": video_archive.created_at.strftime('%Y-%m-%d'),
+                "start_time": convert_to_local_time(video_archive.start_time, timezone_str=timezone_str).strftime("%H:%M:%S"),
+                "end_time": convert_to_local_time(video_archive.end_time, timezone_str=timezone_str).strftime("%H:%M:%S"),
+                "timestamp": convert_to_local_time(video_archive.created_at, timezone_str=timezone_str).strftime("%H:%M:%S"),
+                "location": plant_entity_localization.title,
+                }
+                
+            rows.append(
+                row
+            )
+        
+        total_record = len(video_archives)
+        results['data'] = {
+            "language": language.name,
+            "tenant": tenant.tenant_name,
+            "type": "collection",
+            "total_record": total_record,
+            "user_filters": lookup_filters.children,
+            "validated_filters": validated_filters,
+            "pages": math.ceil(total_record / items_per_page),
+            "items": rows,
+        }
+        results['status_code'] = "ok"
+        results["detail"] = "data retrieved successfully"
+        results["status_description"] = "OK"
+    
+    except ObjectDoesNotExist as e:
+        results['error'] = {
+            'status_code': "non-matching-query",
+            'status_description': f'Matching query was not found',
+            'detail': f"matching query does not exist. {e}"
+        }
+
+        response.status_code = status.HTTP_404_NOT_FOUND
+        
+    except HTTPException as e:
+        results['error'] = {
+            "status_code": f"{e.status_code}",
+            "status_description": f"{e.detail}",
+            "detail": f"{e.detail}",
+        }
+        
+        response.status_code = e.status_code
+    
+    except Exception as e:
+        results['error'] = {
+            'status_code': 'server-error',
+            "status_description": "Internal Server Error",
+            "detail": str(e),
+        }
+        
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    
+    return results
